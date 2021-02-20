83a4544fa5d81c557c0ca79a38819fb0dbb311eb
Rodrigo Machado Dantas <rmdantas@users.noreply.github.com>
2021-02-20 18:51:45 -0200

    First commit

---
+++ src/main/java/br/com/dantas/TestingSpringBatchApplication.java
@@ -0,0 +1,19 @@
+package br.com.dantas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author rmdantas
+ *
+ */
+
+@SpringBootApplication
+public class TestingSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dantas/TestingSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dantas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
